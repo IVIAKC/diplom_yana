574a6c0430e4e7e1c91e07b45f583e570154c4c7
Unknown <admisas@mail.ru>
2017-05-06 12:26:56 +0700

    Развернул модуль api

---
+++ api/assets/AppAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $depends = [];
+    public $css = [
+    ];
+    public $js = [
+    ];
+
+}

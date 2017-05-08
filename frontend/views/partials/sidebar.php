602928dd40b4dd2a1038cd4d2cc5da9c403fae45
Unknown <admisas@mail.ru>
2017-05-08 18:13:56 +0700

    Footer & header

--- frontend/views/partials/sidebar.php
+++ frontend/views/partials/sidebar.php
@@ -1,11 +1,11 @@
 <div id="sidebar">
     <ul>
-        <li><a href="#">Home</a></li>
+        <li><a href="/project">Home</a></li>
         <li><a href="#">Explore</a></li>
         <li><a href="#">Users</a></li>
         <li><a href="#">Sign Out</a></li>
     </ul>
-    <a href="#" data-toggle="#sidebar" id="sidebar-toggle">
+    <a data-toggle="#sidebar" id="sidebar-toggle">
         <span class="bar"></span>
         <span class="bar"></span>
         <span class="bar"></span>
--- frontend/views/project/index.php
+++ frontend/views/project/index.php
@@ -15,35 +15,10 @@ $this->title = 'Проекты';
 
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
+    
     <p>
         <?= Html::a('Создать проект', false, ['class' => 'btn btn-success', 'id' => 'modal-btn', 'data-target'=>"/report/create"]) ?>
     </p>
-<!--    <table class = "table table-hover">-->
-<!--<!--        TODO Сделать универсальную bootstrap таблицу-->
-<!--        <thead>-->
-<!--        <tr>-->
-<!--            <th>Название</th>-->
-<!--            <th>Заказчик</th>-->
-<!--            <th>Кол-во задач</th>-->
-<!--            <th>Адрес</th>-->
-<!--            <th>Бюджет</th>-->
-<!--        </tr>-->
-<!--        </thead>-->
-<!--        <tbody>-->
-<!--            --><?php //if($data): ?>
-<!--                --><?php //foreach ($data as $item): ?>
-<!--                    <tr>-->
-<!--                        <th>--><?//= $item['name'] ?><!--</th>-->
-<!--                        <th>--><?//= $item['client_id'] ?><!--</th>-->
-<!--                        <th>Тут кол-во задач</th>-->
-<!--                        <th>--><?//= $item['url'] ?><!--</th>-->
-<!--                        <th>--><?//= $item['budget'] ?><!--</th>-->
-<!--                    </tr>-->
-<!--            --><?php //endforeach; ?>
-<!--            --><?php //endif; ?>
-<!--        </tbody>-->
-<!--    </table>-->
     <?php
     yii\bootstrap\Modal::begin([
         'header' => 'Создать проект',
@@ -51,7 +26,6 @@ $this->title = 'Проекты';
         'size' => 'modal-md',
     ]);
     ?>
-<!--    <div id='modal-content'>Загружаю...</div>-->
     <?php yii\bootstrap\Modal::end(); ?>
 
 

20789b53afa0d82f3375549729323aa1873f5e95
Unknown <admisas@mail.ru>
2017-05-09 12:04:05 +0700

    Проставил права.

---
+++ common/models/extended/Client.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace common\models\extended;
+
+use Yii;
+
+/**
+ * This is the model class for table "client".
+ *
+ * @property integer $id
+ * @property string $name
+ *
+ * @property Project[] $projects
+ */
+class Client extends \common\models\Client
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'client';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+        ];
+    }
+}

3aaced43434904abb5fe1b6ac6ea368f7447c5d3
IVIAKC <admisas@mail.ru>
2017-06-11 05:30:02 +0700

    Пробы фронта

--- frontend/controllers/ProjectController.php
+++ frontend/controllers/ProjectController.php
@@ -2,9 +2,9 @@
 
 namespace frontend\controllers;
 
+use common\models\search\ProjectSearch;
 use Yii;
 use common\models\extended\Project;
-use common\models\search\ProjectSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -35,72 +35,13 @@ class ProjectController extends Controller
      */
     public function actionIndex()
     {
-        $model = (new Project());
-        
+        $searchModel = new ProjectSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
-        return $this->render('index', ['data' => $model->getProjectForPrint(), 'model' => $model]);
-    }
-
-    /**
-     * Displays a single Project model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
         ]);
-    }
-
-    /**
-     * Creates a new Project model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Project();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-        return $this->renderAjax('create', [
-            'model' => $model,
-        ]);
-
-    }
-
-    /**
-     * Updates an existing Project model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Project model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
     }
 
     /**
---
+++ frontend/views/project/index.php
@@ -0,0 +1,74 @@
+<?php
+
+use common\models\extended\Client;
+use common\models\extended\Priority;
+use common\models\extended\Status;
+use common\models\extended\Type;
+use common\models\extended\User;
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\search\ProjectSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Проекты';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="project-index">
+
+    <h1><?=Html::encode($this->title) ?></h1>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'name',
+            'description:ntext',
+
+            [
+                'attribute' => 'lead_id',
+                'format' => 'html',
+                'value' => function($data){
+                    return Html::a($data->lead->username,['user/view', 'id' => $data->lead_id]);
+                },
+                'filter' => User::getUserList(),
+
+            ],
+            [
+                'attribute' => 'status_id',
+                'format' => 'html',
+                'value' => function($data){
+                    return Html::a($data->status->name,['status/view', 'id' => $data->status_id]);
+                },
+                'filter' => Status::getStatusList(),
+            ],
+
+            [
+                'attribute' => 'priority_id',
+                'format' => 'html',
+                'value' => function($data){
+                    return Html::a($data->priority->name,['priority/view', 'id' => $data->priority_id]);
+                },
+                'filter' => Priority::getPriorityList(),
+
+            ],
+
+            [
+                'attribute' => 'is_deleted',
+                'value' => function ($data) {
+                    return $data->is_deleted ? 'Да' : 'Нет';
+                },
+                'filter' => [
+                    0 => 'Нет',
+                    1 => 'Да',
+                ]
+            ],
+            'created_at',
+            'updated_at',
+
+        ],
+    ]); ?>
+</div>
